f93493b130b2ab1db191409d17b6c4d8ba836c6e
Mike K <m.kermes89@gmail.com>
2018-10-11 18:49:59 -0700

    console logs return text instead of binary data

--- edit-file.js
+++ edit-file.js
@@ -1,5 +1,28 @@
 'use strict';
 
 const fs = require('fs');
+const faker = require('faker');
+//const file = process.argv[2];
+let content;
 
-fs;
+fs.readFile('./files/test.txt', 'UTF-8', function read(err, data){
+  if(err){
+    throw err;
+  }
+  content = data;
+  console.log(content);
+  processFile();
+});
+
+function processFile() {
+  console.log(content);
+}
+
+/* fs.appendFile('./files/test.txt',` ${faker.random.word()}`, function(err) {
+  if(err){
+    throw err;
+  }
+  console.log(process.argv[2]);
+}); */
+
+console.log('after calling readfile');
